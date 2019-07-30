5079928661754b44be9415010437f12a562d0f48
lpssudarshana <id.etisalat@gmail.com>
2019-07-30 12:48:08 +0530

    changed the handler

--- sigma-express-test/lambda.js
+++ sigma-express-test/lambda.js
@@ -1,19 +1,15 @@
 const serverless =  require('serverless-http');
 const express = require('express');
 
-
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('sigma express success')
 })
 
-
-
-
 exports.handler = function(event, context, callback) {
     
-    event = app;
+    event = serverless(app);
 }
 
 
