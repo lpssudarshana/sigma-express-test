eaa8aecd4720793e8ff6ae9e8cf0615faf6f162a
lpssudarshana <id.etisalat@gmail.com>
2019-07-30 12:45:56 +0530

    initial commit

---
+++ sigma-express-test/lambda.js
@@ -0,0 +1,19 @@
+const serverless =  require('serverless-http');
+const express = require('express');
+
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('sigma express success')
+})
+
+
+
+
+exports.handler = function(event, context, callback) {
+    
+    event = app;
+}
+
+
